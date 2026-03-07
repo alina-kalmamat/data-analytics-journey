b02c3b521e11e517458de2687a4d1e8ef35c70b6
Alina Kalmamatova <alkalmamatova@gmail.com>
2026-03-07 11:21:25 +0100

    Add Week 4 SQL concepts and project details
    Added comments outlining the concepts and platforms for Week 4.

---
+++ SQL_Exercises/Week_4.sql
@@ -0,0 +1,7 @@
+/* WEEK 4: Advanced SQL & Completion
+   Concepts:
+   - Offset Functions: LAG, LEAD (Time-series comparison)
+   - Common Table Expressions: WITH (CTE) for modular queries
+   Platforms: [SQL Academy](https://sql-academy.org/ru/trainer), [Kaggle](https://www.kaggle.com/learn/advanced-sql)
+   Final Milestone: Northwind Database Project completed
+*/
